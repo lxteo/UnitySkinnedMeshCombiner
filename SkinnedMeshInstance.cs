2e26260974cbc9d5992a23a14b1d96f9b6b889bf
lxteo <lxteo@users.noreply.github.com>
2019-04-09 21:52:41 +0800

    Create SkinnedMeshInstance.cs

---
+++ SkinnedMeshInstance.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SkinnedMeshInstance
+{
+    public Mesh Mesh { get; set; }
+    public SkinnedMeshRenderer SMR { get; set; }
+    public int SubMeshIndex { get; set; }
+    public Matrix4x4 Transform { get; set; }
+    public Material Material { get; set; }
+    public Animator Animator { get; set; }
+}
